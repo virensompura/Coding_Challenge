d6f5952d4c49f26d6cd67ef6aaf1d2947c3634f7
virensompura <100958586+virensompura@users.noreply.github.com>
2023-10-04 17:09:51 +0530

    Create multi_input_dict.py

---
+++ multi_input_dict.py
@@ -0,0 +1,13 @@
+dict = {}
+
+
+n = int(input("Please enter the number of elements in the dictionary: "))
+
+
+for i in range(n):
+    x = int(input("Please enter the value for x in element {}: ".format(i+1)))
+    y = int(input("Please enter the value for y in element {}: ".format(i+1)))
+    z = int(input("Please enter the value for z in element {}: ".format(i+1)))
+    dict[x, y, z] = x + y - z
+
+print(dict)
